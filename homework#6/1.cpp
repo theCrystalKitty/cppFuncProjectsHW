742ac0b938bb32995b7c79c0f8487ccfbc01940f
CrystalKitty <103777860+theCrystalKitty@users.noreply.github.com>
2023-11-14 07:41:13 +0300

    Create 1.cpp

---
+++ homework#6/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    
+    int* nums = new int[10];
+    int min = INT32_MAX, max = INT32_MIN;
+    short indexMin, indexMax;
+
+    for (short i = 0; i < 10; i++) {
+        nums[i] = 1 + rand() % 10;
+    }
+
+    for (short i = 0; i < 10; i++) {
+        if (min >= nums[i])
+            min = nums[i];
+        if (max <= nums[i])
+            max = nums[i];
+        cout << nums[i] << ' ';
+    }
+
+    cout << endl << "Min: " << min << endl << "Max: " << max;
+
+    return 0;
+}
