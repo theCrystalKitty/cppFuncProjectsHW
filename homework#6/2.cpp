2e2cd0f5c8c6564d21d291065fa439b8ab6c444a
CrystalKitty <103777860+theCrystalKitty@users.noreply.github.com>
2023-11-14 07:46:13 +0300

    Create 2.cpp

---
+++ homework#6/2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    
+    int* nums = new int[10];
+    int rangeStart, rangeEnd, num, sum = 0;
+    short indexMin, indexMax;
+
+    cout << "Input a range: ";
+    cin >> rangeStart >> rangeEnd;
+    cout << "Input a number: ";
+    cin >> num;
+
+    for (short i = 0; i < 10; i++) {
+        nums[i] = rangeStart + rand() % (rangeEnd + 1 - rangeStart);
+    }
+
+    for (short i = 0; i < 10; i++) {
+        if (nums[i] < num)
+            sum += nums[i];
+        cout << nums[i] << ' ';
+    }
+
+    cout << endl << "Sum: " << sum;
+
+    return 0;
+}
