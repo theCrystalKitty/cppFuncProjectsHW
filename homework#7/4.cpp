3dbca5207fb58a40be97a96ba272ba5ffd00f3e4
CrystalKitty <103777860+theCrystalKitty@users.noreply.github.com>
2023-11-21 07:51:00 +0300

    Create 4.cpp

---
+++ homework#7/4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float* usdToEur = new float[12];
+    float* percent = new float[12];
+    float deposit;
+
+    for (short i = 0, j = 0; i < 12; i++) {
+        cout << "Input usd to eur conversion for " << i + 1 << " month: ";
+        cin >> j;
+        usdToEur[i] = j;
+    }
+
+    for (short i = 0, j = 0; i < 12; i++) {
+        cout << "Input percents added for " << i + 1 << " month: ";
+        cin >> j;
+        percent[i] = j;
+    }
+
+    cout << "Input you deposit amount: ";
+    cin >> deposit;
+
+    short month;
+
+    cout << "Input a month: ";
+    cin >> month;
+
+    for (short i = 11, j = deposit; i >= month - 1; i--) {
+        if (i == month) {
+            if (j / 100 * percent[i] >= 500) {
+                cout << "You can withdraw: $" << (j / 100 * percent[i]) / 2 << endl;
+                break;
+            }
+            else
+                break;
+        }
+        j -= j / 100 * percent[i];
+    }
+
+    return 0;
+}
